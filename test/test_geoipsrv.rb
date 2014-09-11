de0a9fa89597829dae4cc4b58555428e16af1761
shadowbq <shadowbq@gmail.com>
2014-09-11 11:00:17 -0400

    added minitest/reporters

--- test/test_geoipsrv.rb
+++ test/test_geoipsrv.rb
@@ -6,7 +6,10 @@ require 'rack/test'
 require 'geoip_server'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'shoulda/context'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
 
 class GeoipServerTest < Minitest::Test
   include Rack::Test::Methods
